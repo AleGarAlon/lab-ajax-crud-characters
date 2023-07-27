08992f2a0ac9aa4d8293045dc768cb3d59ce6b37
Alejandro <alegaralonrgmail.com>
2023-07-27 16:15:54 +0200

    inProgress

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -3,23 +3,52 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+ async getFullList  ()   {
+        const response = await axios.get("http://127.1.1.0:8000/characters")
+
+  if (response.status === 200) {
+    return response.data
+    }
+  }
+
+  async getOneRegister () {
+          const response = await axios.get("http://127.1.1.0:8000/characters/:id")
+
+    if (response.status === 200) {
+      return response.data
+
+      }
 
   }
 
-  getOneRegister () {
-
+  async createOneRegister (characterInfo) {
+      try {
+        const response = await axios
+        .post("http://127.1.1.0:8000/characters", characterInfo)
+         console.log(characterInfo)
+         return response.data
+       }
+        catch (error) {
+        console.log(error)
+      }
   }
 
-  createOneRegister () {
-
+  async updateOneRegister (characterNewInfo) {
+    try {const response = await axios
+      .patch("http://127.1.1.0:8000/characters/:id", characterNewInfo)
+      
+       return response.data
+     }
+      catch (error) {
+      console.log(error)
+    }
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  async deleteOneRegister () {
+    if (response.status === 200) {
+      const response = await axios.delete("http://127.1.1.0:8000/characters/:id")
+       
+      }
   }
 }
+
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,38 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('http://127.0.0.1:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    try{
+     const allCharacters = await charactersAPI.getFullList()  
+     return allCharacters
+    }catch (error){
+      console.log(`Your error is: ${error}`)
+    }
+     /*allCharacters.forEach((character) => {
+      character.name = document.getElementsByClassName("name")
+     })
+     console.log(allCharacters)   */
+  });
+
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+
+    try{
+    const oneCharacter = await charactersAPI.getOneRegister()
+    }
+    catch (error){
+      console.log(`Your getElementById error is: ${error}`)
+    }
+  });
+
+  document.getElementById('delete-one').addEventListener('click', async function (event) {
 
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document.getElementById('edit-character-form').addEventListener('submit', async function (event) {
 
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document.getElementById('new-character-form').addEventListener('submit', async function (event) {
 
   });
 });
